2c4b7cc023278a4ed9a966fa033b2e068f3b16b0
BrunoValsecchi <cai.bruno.valsecchi@gmail.com>
2025-04-09 17:16:00 -0300

    traer jugadores

--- myapp/views.py
+++ myapp/views.py
@@ -8,6 +8,20 @@ def home(request):
     sofascore = sfc.Sofascore()
     transfermarkt=sfc.Transfermarkt()
 
+
+
+    player_url='https://www.sofascore.com/es/jugador/lionel-messi/12994'
+    stats_player = sofascore.scrape_player_league_stats(
+        '2025',
+        'Argentina Liga Profesional',
+        'total',
+        ['Forwards']
+    )
+
+    print(stats_player.head())  
+
+   
+
     match_url = 'https://www.sofascore.com/es/football/match/real-madrid-arsenal/RsEgb#id:13513403'
     match_data = sofascore.get_match_dict(match_url)
     
@@ -25,6 +39,22 @@ def home(request):
         'arbitro': match_data['referee']['name'],
     }
 
+    match_url2 = 'https://www.sofascore.com/es/football/match/barcelona-borussia-dortmund/ydbsrgb#id:13513404'
+    match_data2 = sofascore.get_match_dict(match_url2)
+    
+    
+    
+    info2 = {
+        'torneo': match_data2['tournament']['name'],
+        'ronda': match_data2['roundInfo']['name'],
+        'fecha': datetime.datetime.fromtimestamp(match_data['startTimestamp']).strftime('%d/%m/%Y %H:%M'),
+        'equipo_local': match_data2['homeTeam']['name'],
+        'equipo_visitante': match_data2['awayTeam']['name'],
+        'estadio': match_data2['venue']['name'],
+        'ciudad': match_data2['venue']['city']['name'],
+        'arbitro': match_data2['referee']['name'],
+    }
+
    # class InvalidYearException(Exception):
     #    def __init__(self, year, league, valid_years):
      #       message = f"Año inválido: {year} para la liga {league}. Años válidos: {', '.join(valid_years)}"
@@ -40,39 +70,12 @@ def home(request):
         #tablas_url=transfermarkt.comps[league].replace
         
 
-    def get_current_season_id(tournament_id):
-        url = f"https://api.sofascore.com/api/v1/unique-tournament/{tournament_id}/season/active"
-        response = requests.get(url)
-        data = response.json()
-        return data['season']['id']
-
     
-    def get_tablas(tournament_id):
-        season_id = get_current_season_id(tournament_id)
-        url = f"https://api.sofascore.com/api/v1/unique-tournament/{tournament_id}/season/{season_id}/standings/total"
-        response = requests.get(url)
-        return response.json()
+    context = {
+        'stats': stats_player.to_dict(orient='records'),
+        'player_url': player_url,
+        'info': info,
+        'info2': info2
+    }
 
-    
-    def parse_tablas(data):
-        rows = []
-        standings = data['standings'][0]['rows']
-        for team in standings:
-            rows.append({
-                'position': team['position'],
-                'team': team['team']['name'],
-                'played': team['matches'],
-                'wins': team['wins'],
-                'draws': team['draws'],
-                'losses': team['losses'],
-                'goals_for': team['scoresFor'],
-                'goals_against': team['scoresAgainst'],
-                'points': team['points']
-            })
-        return pd.DataFrame(rows)
-    data = get_tablas(155)  l
-    df = parse_tablas(data)
-    print(df)
-    tabla = df.to_dict(orient='records')  
-
-    return render(request, 'index.html', {'info': info, 'tabla': tabla})
+    return render(request, 'index.html',context)
