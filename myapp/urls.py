a3921b5b0e0ebbcc2957da27c8475a64e112cf99
BrunoValsecchi <cai.bruno.valsecchi@gmail.com>
2025-04-29 12:16:42 -0300

    creando grafico

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views  # o from .views import home
+from .views import grafico
 
 urlpatterns = [
-    path('', views.home, name='home'),  
+    path('', views.home, name='home'), 
+    path('grafico/', views.grafico, name='grafico'),
+ 
 ]
